92e21f5b96c731ac19ea21791c1ba813bd096bb7
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2026-08-29 19:06:43 +0600

    lh
    Signed-off-by: Nahidujjaman Hridoy <hridoyboss12@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -153,7 +153,7 @@ export default function RootLayout({
           {/* <Header /> */}
           {/* <div className="bg-grain" /> */}
           <ReadingProgress />
-          <Intro />
+          {/* <Intro /> */}
           {children}
           <Footer />
         </Providers>
