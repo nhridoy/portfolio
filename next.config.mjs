08c209b00f8c0385a178e8513537016d307465c0
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2026-08-30 12:35:49 +0600

    headers
    Signed-off-by: Nahidujjaman Hridoy <hridoyboss12@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -31,6 +31,32 @@ const nextConfig = {
     inlineCss: true,
     // optimizeCss: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=(), usb=()",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "upgrade-insecure-requests",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const withNextra = nextra({
